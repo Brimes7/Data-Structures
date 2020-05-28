49559ae145f0b3d147661ff980e88f50ab42a037
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-05-27 21:35:29 -0500

    Coding in Class GP

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,12 +17,32 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+
+        if value >= self.value:
+            #check if dot right exists.
+            if self.right is not None:
+            self.right.insert(value)
+        else:
+        new_node = BSTNode(value)
+        self.right = new_node
+
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if self.value == target:
+            return True
+
+        elif target >= self.value:
+            if self.right is not None:
+                return self.right.contains(target)
+            else:
+                return False
+        else:
+            if self.left is not None #we have a left child
+                return self.left.contains(target)
+            else:
+                return False
 
     # Return the maximum value found in the tree
     def get_max(self):
